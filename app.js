eaa7c77f1b45a74f08f0c461a5a81dc95c9ff9c3
Your Name <heshamyuosif@gmail.com>
2020-09-09 14:54:06 +0200

    Full backend-50% fornt end

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+
+const mongoose = require('mongoose');
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+const isAuth = require('./middleware/is-auth')
+
+app.use(bodyParser.json());
+app.use((req,res ,next) =>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    if (req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
+app.use(isAuth);
+
+app.use(
+    '/graphql',
+    graphqlHttp({
+    schema: graphqlSchema,
+    rootValue:graphqlResolvers,
+    graphiql: true
+
+}));
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jeiyi.gcp.mongodb.net/booking-app?retryWrites=true&w=majority`,{ useUnifiedTopology: true ,useNewUrlParser: true } )
+.then(()=>
+    app.listen(8000,(req,res) => {
+      console.log("Server is up and running!!..");
+})
+
+).catch(err => {
+    console.log(err);
+});
+
